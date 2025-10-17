5220f855f30291c1f9e908dc6d7378899bbc8b2b
bara-codes <198811843+bara-codes@users.noreply.github.com>
2025-10-17 08:10:09 -0400

    Implement palindrome checking from user input
    Added functionality to check if a word is a palindrome based on user input.

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+        int length = word.length();
+        String reverse = "";
+        for (int j =0; j<length; j++)
+            {
+                reverse = reverse + word.charAt(length-j-1);
+            }
+        if (word.equals(reverse))
+        {
+            System.out.println(word + " is a palindrome!");
+        }
+        else
+        {
+            System.out.println(word + " is NOT a palindrome.");
+        }
+    }
+}
+            
         // TODO: Create a Scanner and read a full line from the user with nextLine().
         //       Keep the original input string so you can print it in the final message.
 
